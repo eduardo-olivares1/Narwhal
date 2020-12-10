a3ac1e75827287e183636fd431fc3dc4b4af369f
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-12-09 20:37:25 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Import environment variables 
+const dotEnv = require('dotenv').config();
+const discordToken = process.env.DISCORD_TOKEN;
+const twitterToken = process.env.TWITTER_TOKEN;
+
+const Discord = require('discord.js'); 
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Narwhal is online!');
+});
+
+client.login(discordToken);
