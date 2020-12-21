49d0ed375f74947f8f1a25b4bd5fe9e32f27c734
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-12-21 14:15:19 -0800

    Added ping command

---
+++ commands/ping.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+module.exports = {
+    name: 'ping',
+    description: "A simple ping command to check if API services are active",
+    execute(message, args){ 
+        if (args[0].toLowerCase() === "coingecko"){
+            request('https://api.coingecko.com/api/v3/ping', { json: true },  function(err, res, body) {
+                message.channel.send("CoinGecko says: " + body['gecko_says']);
+            });
+            return;
+        }
+
+    }
+}
