65cf0c5c27ff00f098d4d0a8f51ebcdad73b7032
Eduardo Olivares <eduardo.olivares@sjsu.edu>
2020-12-21 14:12:26 -0800

    Added required dependencies + env vars

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-//Import environment variables 
+//Import required dependencies + files
 const dotEnv = require('dotenv').config();
+const Discord = require('discord.js');
+const fs = require('fs');
+
+//Import environment variables 
 const discordToken = process.env.DISCORD_TOKEN;
-const twitterToken = process.env.TWITTER_TOKEN;
 
-const Discord = require('discord.js'); 
-
+//Create Discord Client
 const client = new Discord.Client();
 
-client.once('ready', () => {
-    console.log('Narwhal is online!');
+//Set Up Commands
+const prefix = "$"; 
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () =>{
+    console.log('Bot is online!');
+});
+
+
+client.on('message', message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    }
 });
 
 client.login(discordToken);
